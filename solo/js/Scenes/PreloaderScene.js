1810232a49eee2c1e90bbd4afd250737adbd2a8a
V-Teck <vkachurin@inbox.ru>
2019-09-25 12:57:07 +0300

    Исправлен запуск сцены меню после загрузки ресурсов

--- solo/js/Scenes/PreloaderScene.js
+++ solo/js/Scenes/PreloaderScene.js
@@ -10,7 +10,7 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   preload () {
-    // add logo image
+    console.log('add logo image');
     this.add.image(400, 200, 'logo');
 
     // display progress bar
@@ -79,17 +79,19 @@ export default class PreloaderScene extends Phaser.Scene {
 
     this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
-    // load assets needed in our game
+    console.log('load assets needed in our game');
     this.load.image('blueButton1', 'assets/ui/blue_button02.png');
     this.load.image('blueButton2', 'assets/ui/blue_button03.png');
     this.load.image('GameLogo', 'assets/Image.png');
     this.load.image('box', 'assets/ui/grey_box.png');
     this.load.image('checkedBox', 'assets/ui/grey_boxCheck.png');
+    console.log('bgM');
     this.load.audio('bgMusic', ['assets/TownTheme.mp3']);
+    console.log('asset list end');
   }
 
   ready () {
-    this.scene.start('Title');
+    //this.scene.start('Title');
     this.readyCount++;
     if (this.readyCount === 2) {
       this.scene.start('Title');
